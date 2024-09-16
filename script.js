045d7a8c608f96426bd087979d97b4fddbfa2365
kogilo <getabullaothow@gmail.com>
2024-09-16 13:34:37 -0500

    validation steps added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Plan 
-    // -01- Input Data Validation - make sure that `AssignmentGroup` belong to `CourseInfo`,
+    // -01- Create each data: 
+    // -02- Input Data Validation - make sure that `AssignmentGroup` belong to `CourseInfo`,
     // throw error if incorrect data type and handle division by zero, if `points_possible` is zero.
-   // 02 - Deadlines and penalties: 
+   // 03 - Deadlines and penalties: 
     // - If the assignment is not yet due, exclude it
     // - If the learner submission is late, deduct 10% from his/her result.
-// 03 - Calculate average score: calculate the weighted average for learner score. Assignments with more points should weight more.
+// 04 - Calculate average score: calculate the weighted average for learner score. Assignments with more points should weight more.
 
-// 04- Return the output in the following format"
+// 05- Return the output in the following format"
 
 
 /* 
@@ -26,3 +27,142 @@
     // the average or the keyed dictionary of scores
 }
     */
+
+// -01- Create each data: 
+// ====From STARTER CODE====
+
+// The provided course information.
+const courseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const assignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const learnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+
+
+  function getLearnerData(course, ag, submissions) {
+    // here, we would process this data to achieve the desired result.
+    const result = [
+      {
+        id: 125,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0 // 150 / 150
+      },
+      {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+      }
+    ];
+  
+    return result;
+  }
+  
+  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+  console.log(result);
+  
+
+  // -02- Input Data Validation - make sure that `AssignmentGroup` belong to `CourseInfo`,
+function validateAssignmentGroup(course, AssignmentGroup) {
+    if (AssignmentGroup.course_id !== course.id) {
+        throw new Error(`Assignment group does not belong to the provided course.`)
+
+    }
+
+}
+
+
+// throw error if incorrect data type and handle division by zero, if `points_possible` is zero.
+
+function validateLearnerSubmissions(submission) {
+    if(typeof submission.score !== "number" || isNaN(submission.score)) {
+        throw new Error(`Invalid submission score data`);
+    }
+
+}
+
+// 03 - Deadlines and penalties: 
+function applyLateSubmPenality(submissionDate, dueDate, score, pointsPossible) {
+    const submitted = new Date(submissionDate);
+    const due = new Date(dueDate);
+    
+    if (submitted > due) {
+        // 10% penalty for late submission
+        return score - 0.1 * pointsPossible;
+      }
+      return score;
+
+}
